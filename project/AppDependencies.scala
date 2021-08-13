3968f332c5bc28a7637376765c22215d47a27f8c
Joao Maio <10330349+JMaio@users.noreply.github.com>
2021-08-13 09:18:01 +0100

    DDCE-2115: update to play 2.8, bump dependencies
    pins ruby version to `2.6.0` as per e84652f2e0690935494b5e490b12c236e3c6f6a3

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "5.3.0"
+  val bootStrapPlayVersion = "5.10.0"
   val silencerVersion = "1.7.1"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % bootStrapPlayVersion,
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion,
     compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full
   )
