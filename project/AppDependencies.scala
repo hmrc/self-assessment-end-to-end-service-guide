b82ca0353bcb9e68cd4de96c1793179e93858dff
Lorenzo Mugnai <11977337+lmugnai@users.noreply.github.com>
2023-02-28 11:18:28 +0000

    DDCE-3593: Upgrade Scala + other deps (#44)
    * DDCE-3593: Upgrade Scala + other deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,15 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "7.1.0"
-  val silencerVersion = "1.7.9"
+  val bootStrapPlayVersion = "7.14.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion,
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
-    "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion
   )
 
   val test = Seq(
     "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % Test,
-    "org.scalatest" %% "scalatest" % "3.2.13" % Test)
+    "org.scalatest" %% "scalatest" % "3.2.15" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootStrapPlayVersion % Test)
 }
