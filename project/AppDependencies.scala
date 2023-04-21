ff66b7fc69774b816f216d75337d92766464b1a4
EdCope <87376121+EdCope@users.noreply.github.com>
2023-04-21 12:04:03 +0100

    DDCE-3741: Improve CSP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,16 +1,16 @@
-import play.sbt.PlayImport._
-import sbt._
+import play.sbt.PlayImport.*
+import sbt.*
 
 object AppDependencies {
-  val bootStrapPlayVersion = "7.14.0"
+  private val bootStrapPlayVersion = "7.15.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion
   )
 
-  val test = Seq(
-    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % Test,
+  val test: Seq[ModuleID] = Seq(
+    "com.vladsch.flexmark" % "flexmark-all" % "0.64.0" % Test,
     "org.scalatest" %% "scalatest" % "3.2.15" % Test,
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootStrapPlayVersion % Test)
 }
