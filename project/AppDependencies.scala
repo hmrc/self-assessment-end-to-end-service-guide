96564ad06b462c41346e5f7ce7aec5805f4d7a9e
Suraj Suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2025-09-12 09:20:40 +0100

    DDCE-6867: self-assessment-end-to-end-service-guide - Implement GOV.UK Rebrand (#87)
    * DDCE-6867: self-assessment-end-to-end-service-guide - Implement GOV.UK Rebrand
    
    * DDCE-6867: self-assessment-end-to-end-service-guide - Implement GOV.UK Rebrand
    
    * DDCE-6867: self-assessment-end-to-end-service-guide - Implement GOV.UK Rebrand

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object AppDependencies {
-  private lazy val bootstrapPlayVersion = "9.15.0"
+  private lazy val bootstrapPlayVersion = "10.1.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion
@@ -11,6 +11,6 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapPlayVersion
   ).map(_ % Test)
 
-  def apply(): Seq[ModuleID]      = compile ++ test
+  def apply(): Seq[ModuleID] = compile ++ test
 
 }
